e3bb7ab845cdead101b5a9be461fb01cfad1d18b
ravisanapala <sanapalaravi@outlook.com>
2022-01-14 18:58:11 +0530

    ts setup

---
+++ typescript/dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+//Function that will multiply two numbers
+function multiply(a, b) {
+    return a * b;
+}
+const a = 10;
+const b = '261';
+console.log(multiply(a, b));
+//Run npx tsc --init for generating tsconfig.json
